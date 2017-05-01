8384ccd6d46ee5783e38db2d548ce056d36be348
takahashi akisato <akisato@takahashi-no-MacBook-Air-2.local>
2017-05-02 01:22:46 +0900

    add config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// ツイッターアプリケーション設定
+define('CONSUMER_KEY', 'test');
+define('CONSUMER_SECRET', 'test');
+define('ACCESS_TOKEN', 'test');
+define('ACCESS_TOKEN_SECRET', 'test');
+
+//定数
+define('TWEET_MAX_COUNT', '100');
